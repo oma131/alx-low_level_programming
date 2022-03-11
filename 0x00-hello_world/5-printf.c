b7b4d782373968e6eaba25b9906424c469341f61
oma131 <anosikeihuoma21@gmail.com>
2022-03-11 09:02:35 -0800

    task 5 commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - function needs to return an integer after execution
+ * Return: return 0 as success and 1 as fail
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
