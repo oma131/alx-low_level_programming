4afab007a5b57856d55944501025650652574d90
oma131 <anosikeihuoma21@gmail.com>
2022-03-11 15:55:12 -0800

    task 4 commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: return 0 as success
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
