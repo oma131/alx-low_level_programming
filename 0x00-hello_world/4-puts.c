1b24fb0bba08517a51e9acd4514da38909d696da
oma131 <anosikeihuoma21@gmail.com>
2022-03-11 08:56:31 -0800

    task 4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * main - function needs to return integer at the end of execution
  * Return: return 0 as success and 1 as fail
  */
-it main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
