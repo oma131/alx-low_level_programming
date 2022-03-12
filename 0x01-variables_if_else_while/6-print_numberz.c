42455b2e579a43775f647f9f243ef8779f69f8c4
oma131 <anosikeihuoma21@gmail.com>
2022-03-11 16:12:46 -0800

    task 6 commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: return 0 as success
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
