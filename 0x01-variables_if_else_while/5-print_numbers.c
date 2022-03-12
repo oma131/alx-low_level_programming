e78bbe896e9c46d5d3b8266f4d8f29cee570de0b
oma131 <anosikeihuoma21@gmail.com>
2022-03-11 16:04:49 -0800

    task 5 commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: return 0 as success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%i", i);
+	putchar('\n');
+
+	return (0);
+}
