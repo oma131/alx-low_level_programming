0e81f968475579a23e85fc7234d085d905f0de04
oma131 <anosikeihuoma21@gmail.com>
2022-03-11 16:51:05 -0800

    task 7 commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main -Entry Point
+ * Description: prints single digit numbers seperated by comma and space
+ * Return: return 0 as success
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num + '0')
+		if (num < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
