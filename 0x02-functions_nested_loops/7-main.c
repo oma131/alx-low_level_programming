583612ccf5859e8224001f321d93e3bfdc0384dd
oma131 <anosikeihuoma21@gmail.com>
2022-03-16 15:01:46 -0700

    task 7 commit

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a number
+ * @n: int type number
+ * Return: return 0 as success
+ */
+int print_last_digit(int n)
+{
+	int r;
+
+	print_last_digit(98);
+	print_last_digit(0);
+	r = print_last_digit(-1024);
+	_putchar('0' + r);
+	_putchar('\n');
+return (0);
+}
