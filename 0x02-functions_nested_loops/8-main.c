6be49392743fdc79b9975271beaaa03a00115f24
oma131 <anosikeihuoma21@gmail.com>
2022-03-16 15:17:41 -0700

    task 8 commit

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints time
+ *
+ * Description: prints a list of time
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	jack_bauer();
+	return (0);
+}
