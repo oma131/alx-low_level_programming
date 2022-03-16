23772c96a6dbe4c29bf5b3649d386b57929b84fd
oma131 <anosikeihuoma21@gmail.com>
2022-03-15 22:07:26 -0700

    task 1 commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - a function that prints alphabet in lowercase.
+ * Return: return 0 as success
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
