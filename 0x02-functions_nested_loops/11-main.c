eb4d65b78119fda7e2322ed98bc703ac40fa7251
oma131 <anosikeihuoma21@gmail.com>
2022-03-16 18:09:13 -0700

    task 11 commit

---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - a function that prints all natural numbers from n to 98
+ * user input's number prints to 98, regardless < 98 or > 98
+ * @n: number input
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+return (0);
+}
