d85031c5b9a971219535e13aa129565a5bfee56d
oma131 <anosikeihuoma21@gmail.com>
2022-03-16 17:52:17 -0700

    task 10 commit

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - add two numbers from input
+ * @a: first aparamet
+ * @b: second parameter
+ *
+ * Description: adds two numbers
+ * Return: Always (0).
+ */
+
+int add(int a, int b)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+return (0);
+}
