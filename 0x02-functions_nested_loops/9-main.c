c56398f1c9082ac637fbe95ad8a9115f2b934c52
oma131 <anosikeihuoma21@gmail.com>
2022-03-16 17:40:19 -0700

    task 9 commit

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * times_table -  a function that prints the 9 times table, starting with 0
+ * rone = row, cone = column, d = digits of current result
+ * Return: times table
+ * add extra space past single digit
+ */
+
+void times_table(void)
+{
+	times_table();
+	return (0);
+}
