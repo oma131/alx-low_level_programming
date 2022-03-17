f1ca6f1f1e13f04c330bbc71e2e0ebb9278d05f3
oma131 <anosikeihuoma21@gmail.com>
2022-03-17 05:20:03 -0700

    task commit

---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: return 0 as success
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int a, b, c;
+	int largest;
+
+	a = 972;
+	b = -98;
+	c = 0;
+
+	largest = largest_number(a, b, c);
+
+	printf("%d is the largest number\n", largest);
+
+	return (0);
+}
