432ef3a7932544163f7fb5de4496d81763284a2b
oma131 <anosikeihuoma21@gmail.com>
2022-03-16 18:45:08 -0700

    task 12 commit

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
+ */
+
+void print_times_table(int n)
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+return (0);
+}
