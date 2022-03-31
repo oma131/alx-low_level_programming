28c40365b050ead6802f95693d73aba4be19113c
oma131 <anosikeihuoma21@gmail.com>
2022-03-30 22:10:20 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * isSqrt - helper function for _sqrt_recursion
+ * @n: input number
+ * @i: iteratotr
+ * Return: square root of number
+ */
+
+int find_root(int n, int root)
+{
+	if (root * root > n)
+		return (-1);
+
+	if (root * root == n)
+		return (root);
+
+	return (find_root(n, root + 1));
+}
+
+/**
+ * _sqrt_recursion - prints square root of a function
+ * @n: Parameter passed
+ *
+ * Return: 0 as success
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n == 0 || n == 1)
+		return (n);
+
+	return (find_root(n, 0));
+}
